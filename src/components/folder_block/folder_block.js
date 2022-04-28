43d57d87eea50abae623e53dc1ea54f4e5402efb
Aleksandr232 <aleksrowing232@gmail.com>
2022-04-29 02:48:36 +0300

    icons

--- src/components/folder_block/folder_block.js
+++ src/components/folder_block/folder_block.js
@@ -3,12 +3,30 @@ import React from "react";
 import './folder_block.scss'
 
 import folder from './folder.png'
+import bluere from './bluere.png'
+import recta from './recta.png'
+import rectan from './rectan.png'
 
  const FolderBlock=()=>{
     return(
         <section>
             <div className="sort">sort by name</div>
-            <div className="blockmenu"></div>
+            <div className="blockmenu">
+                <div className="bluer">
+                    <img className="bluere" src={bluere} alt="" />
+                    <img className="bluere" src={bluere} alt="" />
+                    <img className="bluere" src={bluere} alt="" />
+                    <img className="bluere" src={bluere} alt="" />
+                </div>
+                <div className='recta'>
+                    <img src={recta} alt="" />
+                    <img src={rectan} alt="" />
+                    <img src={recta} alt="" />
+                    <img src={rectan} alt="" />
+                    <img src={recta} alt="" />
+                    <img src={rectan} alt="" />
+                </div>
+            </div>
             <div className="newfoldergrid">
                 <div className="block">
                     <div className="img">
--- src/components/headers/headers.js
+++ src/components/headers/headers.js
@@ -4,6 +4,9 @@ import './headers.scss'
 import logo from './logo.png'
 import vector from './Vector.png'
 import vector1 from './vector1.png'
+import vector15 from './vector15.png'
+import vector16 from './Vector16.png'
+import vector17 from './Vector17.png'
 
 
  const Headers=()=>{
@@ -12,7 +15,8 @@ import vector1 from './vector1.png'
             <div className="btn" >
             <button className="btn1" style={{width:'138px', height:'40px', borderRadius:'54px', background: '#6965E1', color:'#ffff'}}>UPLOAD <div className="round"><div className="plus"></div><div className="plus2"></div></div></button>
             <button className="btn2"style={{width:'40px', height:'40px', borderRadius:'71px'}}> <img className="vector1" src={vector1}></img><img className="vector2" src={vector}/></button>
-            <button className="btn3" style={{width:'40px', height:'40px', borderRadius:'71px'}}></button>
+            <button className="btn3" style={{width:'40px', height:'40px', borderRadius:'71px'}}> <img className="vector15" src={vector15} alt="" /><img className="vector16" src={vector16} alt="" />
+            <img className="vector17" src={vector17} alt="" /></button>
             </div>
             <div className="logo">
                 <img src={logo} alt=""/>
